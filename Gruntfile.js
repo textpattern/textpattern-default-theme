d54a45b3a28cbcbe56264022df8a514f0399f6ef
Phil Wareham <phil@designhive.com>
2017-05-08 09:56:30 +0100

    Autoprefixer now uses global browser config

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,12 +83,7 @@ module.exports = function (grunt)
         postcss: {
             options: {
                 processors: [
-                    require('autoprefixer')({
-                        browsers: [
-                            'last 3 versions',
-                            'not ie <= 11'
-                        ]
-                    })
+                    require('autoprefixer')
                 ]
             },
             files: {
