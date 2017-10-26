931099abc35f0df4cf27f98f096152a52ac60f52
makss <makss@users.noreply.github.com>
2017-10-26 13:26:06 +0300

    AutoBuild/Repair manifest.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
-//path = require('path');
-//var fs = require('fs');
+var fs = require('fs');
 
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
-//var WebpackOnBuildPlugin = require('on-build-webpack');
+var WebpackOnBuildPlugin = require('on-build-webpack');
 
 module.exports = {
-    context: __dirname,
     entry: {
         'styles/default.css': './src/scss/default.scss'
     },
@@ -39,11 +37,30 @@ module.exports = {
                 from: '**/*',
             }
         ]),
-//        new WebpackOnBuildPlugin(function(stats) {
-//            console.log(stats);
-//var stat_out = JSON.stringify(module, '_', 4);
-//fs.writeFile('helloworld.txt', stat_out);
-//fs.unlinkSync('wqqwqwq');
-//        }),
+        new WebpackOnBuildPlugin(function(stats) {
+            // Delete `output.filename`
+            try {
+                fs.unlinkSync(this.outputPath+'/'+this.options.output.filename);
+            } catch(e) {
+            }
+
+            // AutoBuild/Repair `manifest.json` from `package.json`, if some parameters are missing or `manifest.json` not exist
+            var pkg = require(this.context+'/package.json');
+            try {
+                var manifest = require(this.outputPath+'/manifest.json');
+            } catch(e) {
+                var manifest = {};
+            }
+
+            var manifest2 = {
+                "title":        manifest.title          || pkg.title        || pkg.name,
+                "txp-type":     manifest['txp-type']    || pkg['txp-type']  || "textpattern-theme",
+                "version":      manifest.version        || pkg.version      || "0.0.1",
+                "description":  manifest.description    || pkg.description  || "",
+                "author":       manifest.author         || pkg.author       || "Team Textpattern",
+                "author_uri":   manifest.author_uri     || pkg.homepage     || pkg.repository.url   || ""
+            };
+            fs.writeFile(this.outputPath+'/manifest.json', JSON.stringify(manifest2, null, 2));
+        }),
     ]
 };
